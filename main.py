04321fed8fe33174e95c147c45175079229d4247
Tomi Chen <tctree333@users.noreply.github.com>
2021-08-16 17:22:06 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import datetime
+import os
+
+import pandas
+import requests
+
+SOURCE_URL = "https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vSJ-xfssimBAcxkv2-yf1tjW7klzc2grFfpR3HZnUaYxMmOi6V7YLSd8vUyOF3sD54CFhPkVbRD1Uz8/pubhtml/sheet?headers=false&gid=652332328"
+
+FILENAME_HTML = datetime.date.today().strftime("data/html/%Y-%m-%d.html")
+FILENAME_CSV = datetime.date.today().strftime("data/csv/%Y-%m-%d.csv")
+FILENAME_DAILY = "data/daily.csv"
+
+
+def main():
+
+    if os.path.exists(FILENAME_HTML):
+        print("file already exists")
+        return
+
+    print("downloading file")
+    resp = requests.get(SOURCE_URL)
+    if resp.status_code != 200:
+        print("error downloading file")
+        return
+
+    html_content = resp.text
+    with open(FILENAME_HTML, "w") as f:
+        f.write(html_content)
+    print("saved file")
+
+    print("converting to csv")
+    dfs = pandas.read_html(FILENAME_HTML, skiprows=3, index_col=0, header=(0, 1))
+    csv = dfs[0].to_csv(index=False)
+    with open(FILENAME_CSV, "w") as f:
+        f.write(csv)
+    with open(FILENAME_DAILY, "w") as f:
+        f.write(csv)
+    print("saved csv")
+
+
+main()
