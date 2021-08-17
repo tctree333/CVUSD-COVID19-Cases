4a05e8b2fcc0e14deb194957e3c6839f9854dc35
Tomi Chen <tctree333@users.noreply.github.com>
2021-08-17 16:16:42 -0700

    update timezones

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import datetime
 import os
+from datetime import datetime, timedelta, timezone
 
 import pandas
 import requests
 
 SOURCE_URL = "https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vSJ-xfssimBAcxkv2-yf1tjW7klzc2grFfpR3HZnUaYxMmOi6V7YLSd8vUyOF3sD54CFhPkVbRD1Uz8/pubhtml/sheet?headers=false&gid=652332328"
 
-FILENAME_HTML = datetime.date.today().strftime("data/html/%Y-%m-%d.html")
-FILENAME_CSV = datetime.date.today().strftime("data/csv/%Y-%m-%d.csv")
+PST_TIME = datetime.now(timezone(-timedelta(hours=8)))
+FILENAME_HTML = PST_TIME.strftime("data/html/%Y-%m-%d.html")
+FILENAME_CSV = PST_TIME.strftime("data/csv/%Y-%m-%d.csv")
 FILENAME_DAILY = "data/daily.csv"
 
 
@@ -24,7 +25,7 @@ def main():
     print("saved file")
 
     print("converting to csv")
-    dfs = pandas.read_html(FILENAME_HTML, skiprows=3, index_col=0, header=(0, 1))
+    dfs = pandas.read_html(html_content, skiprows=3, index_col=0, header=(0, 1))
     csv = dfs[0].to_csv(index=False)
     with open(FILENAME_CSV, "w") as f:
         f.write(csv)
