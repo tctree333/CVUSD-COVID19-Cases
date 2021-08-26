a1ddb4da68bdc2bbf64e8b57f691d8599cf703d8
Tomi Chen <tctree333@users.noreply.github.com>
2021-08-25 19:27:57 -0700

    log data into a spreadsheet

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import hmac
 import os
+import time
 from datetime import datetime, timedelta, timezone
 
 import pandas
 import requests
 
 SOURCE_URL = "https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vSJ-xfssimBAcxkv2-yf1tjW7klzc2grFfpR3HZnUaYxMmOi6V7YLSd8vUyOF3sD54CFhPkVbRD1Uz8/pubhtml/sheet?headers=false&gid=652332328"
+
+LOGGER_DEPLOYMENT_ID = os.environ.get("LOGGER_DEPLOYMENT_ID")
+LOGGER_SECRET = os.environ.get("LOGGER_SECRET")
+LOGGER_URL = "https://script.google.com/macros/s/{deploymentID}/exec?date={date}&studentCases={studentCases}&staffCases={staffCases}&sig={sig}&time={time}"
 
 PST_TIME = datetime.now(timezone(-timedelta(hours=8)))
 FILENAME_HTML = PST_TIME.strftime("data/html/%Y-%m-%d.html")
@@ -25,13 +31,49 @@ def main():
     print("saved file")
 
     print("converting to csv")
-    dfs = pandas.read_html(html_content, skiprows=3, index_col=0, header=(0, 1))
+    dfs = pandas.read_html(html_content, skiprows=3, index_col=1, header=(0, 1))
     csv = dfs[0].to_csv(index=False)
     with open(FILENAME_CSV, "w") as f:
         f.write(csv)
     with open(FILENAME_DAILY, "w") as f:
         f.write(csv)
     print("saved csv")
+
+    print("logging to spreadsheet")
+    current_month_year = PST_TIME.strftime("%B %Y")
+    student_cases = dfs[0].get((current_month_year, "Students"))
+    staff_cases = dfs[0].get((current_month_year, "Staff"))
+
+    if student_cases is None or staff_cases is None:
+        print("error finding cases")
+        return
+
+    current_date = PST_TIME.strftime("%m/%d/%Y")
+    student_case_numbers = student_cases.get("DISTRICT TOTAL:")
+    staff_case_numbers = staff_cases.get("DISTRICT TOTAL:")
+    timestamp = round(time.time())
+    signature = hmac.new(
+        LOGGER_SECRET.encode(),
+        f"{timestamp}|{current_date}|{student_case_numbers}|{staff_case_numbers}".encode(),
+        "sha256",
+    ).hexdigest()
+
+    url = LOGGER_URL.format(
+        deploymentID=LOGGER_DEPLOYMENT_ID,
+        date=current_date,
+        studentCases=student_case_numbers,
+        staffCases=staff_case_numbers,
+        sig=signature,
+        time=timestamp,
+    )
+    print(url)
+    # logger_resp = requests.get(url)
+
+    print(
+        "logged to spreadsheet"
+        if True  # logger_resp.status_code == 200
+        else "error logging to spreadsheet"
+    )
 
 
 main()
